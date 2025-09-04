30a2508f8ff97564d2e98258e521eb87c243b6d1
StephenTeay <127366131+StephenTeay@users.noreply.github.com>
2025-09-04 04:52:58 +0100

    Update ExerciseVideos.js

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { Typography, Box, Stack } from '@mui/material';
-import Loader from './Loader';
+import { Box, Stack, Typography } from '@mui/material';
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
-  if (!exerciseVideos.length) return <Loader />;
+  if (!exerciseVideos.length) return <Typography>No videos available</Typography>;
 
   return (
     <Box sx={{ marginTop: { lg: '203px', xs: '20px' } }} p="20px">
       <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
         Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
       </Typography>
-      <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0px' } }} justifyContent="flex-start" flexWrap="wrap" alignItems="center">
-        {exerciseVideos?.slice(0, 3)?.map((item, index) => (
+      <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: 5, xs: 0 } }} justifyContent="flex-start" flexWrap="wrap" alignItems="center">
+        {exerciseVideos?.slice(0, 6)?.map((item, index) => (
           <a
             key={index}
             className="exercise-video"
             href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
             target="_blank"
             rel="noreferrer"
+            style={{ textDecoration: 'none' }}
           >
-            <img style={{ borderTopLeftRadius: '20px' }} src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <img 
+              style={{ borderTopLeftRadius: '20px' }} 
+              src={item.video.thumbnails[0].url} 
+              alt={item.video.title}
+              width="387px"
+              height="200px"
+            />
             <Box>
               <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color="#000">
                 {item.video.title}
